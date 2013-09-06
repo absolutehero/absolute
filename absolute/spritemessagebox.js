fe356abee61eaf3cef131a5a44f5eb0144d50030
Craig Robinson <craig@absoluteherogames.com>
2013-09-05 21:49:42 -0700

    added button

--- absolute/spritemessagebox.js
+++ absolute/spritemessagebox.js
@@ -3,8 +3,8 @@
  */
 define(['pixi', 'absolute/button', 'absolute/coords'], function(PIXI, Button, Coords) {
 
-    var SpriteMessageBox = function(bgImageName, btnImageName, textStyle, action) {
-        this._initSpriteMessageBox(bgImageName, btnImageName, textStyle, action);
+    var SpriteMessageBox = function(bgImageName, btnImageName, textStyle) {
+        this._initSpriteMessageBox(bgImageName, btnImageName, textStyle);
     };
 
     SpriteMessageBox.constructor = SpriteMessageBox;
@@ -13,15 +13,17 @@ define(['pixi', 'absolute/button', 'absolute/coords'], function(PIXI, Button, Co
     SpriteMessageBox.prototype._initSpriteMessageBox = function(bgImageName, btnImageName, textStyle, action) {
         PIXI.Sprite.call(this, PIXI.Texture.fromFrame(bgImageName));
 
+        this.action = null;
+        this.buttonText = null;
+
         this.button = new Button(
             PIXI.Texture.fromFrame(btnImageName),
             PIXI.Texture.fromFrame(btnImageName),
             function () {
-                Absolute.Debug.log('Dialog button pressed');
-                if (action && typeof action === 'function') {
-                    action();
+                if (this.action && typeof this.action === 'function') {
+                    this.action();
                 }
-            }
+            }.bind(this)
         );
         this.button.position.x = (this.width - this.button.width) / 2;
         this.button.position.y = this.height - this.button.height / 2;
@@ -43,6 +45,22 @@ define(['pixi', 'absolute/button', 'absolute/coords'], function(PIXI, Button, Co
         this.text.position.x = (this.width - this.text.width) / 2;
         this.text.position.y = Coords.y(30);
         this.addChild(this.text);
+    };
+
+    SpriteMessageBox.prototype.setButtonTextAndAction = function(text, action) {
+        if (this.button) {
+            if (this.buttonText) {
+                this.button.removeChild(this.buttonText);
+            }
+
+            this.buttonText = new PIXI.Text(text, this.textStyle);
+            this.buttonText.position.x = (this.button.width - this.buttonText.width) / 2;
+            this.buttonText.position.y = (this.button.height) / 4;
+
+            this.button.addChild(this.buttonText);
+
+            this.action = action;
+        }
     };
 
     return SpriteMessageBox;
