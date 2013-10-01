4f6211998cd9ea7beffb0c3e7d9c1ad158d41f6b
Craig Robinson <craig@absoluteherogames.com>
2013-10-01 10:53:03 -0700

    adding loader

---
+++ absolute/loader.js
@@ -0,0 +1,70 @@
+/**
+ * Created by craig on 9/26/13.
+ */
+define(
+[
+    'pixi',
+    'absolute/platform',
+    'absolute/audiomanager',
+    'absolute/asyncqueue'
+],
+function (
+    PIXI,
+    Platform,
+    AudioManager,
+    AsyncQueue
+    )
+{
+    "use strict";
+
+    var Loader = function () {
+        this._initLoader();
+    };
+
+    Loader.prototype._initLoader = function () {
+
+    };
+
+    Loader.prototype.loadSound = function(assets, onProgress, onComplete) {
+        AudioManager.init(function() {
+            AudioManager.loadSounds(assets, onProgress, onComplete);
+        });
+
+    };
+
+    Loader.prototype.loadArt = function(assets, onProgress, onComplete) {
+        var total = assets.length;
+
+        var paths = [];
+        for (var i = 0; i < total; ++i) {
+            paths.push(Platform.artPathPrefix + '/' + Platform.getResClass() + '/' + assets[i]);
+        }
+
+        this.assetLoader = new PIXI.AssetLoader(paths);
+        this.assetLoader.addEventListener('onComplete', function() {
+            onComplete();
+        });
+        this.assetLoader.addEventListener('onProgress', function(loader) {
+            onProgress(1 - (loader.content.loadCount / total));
+        });
+
+        this.assetLoader.load();
+    };
+
+    Loader.prototype.loadAssets = function (assets, onLoadProgress, onLoadComplete) {
+        var aqueue = new AsyncQueue();
+        aqueue.pushTasks([
+            function (p, c) {
+                this.loadArt(assets.artAssets, p, c);
+            }.bind(this),
+            function (p, c) {
+                this.loadSound(assets.audioAssets, p, c);
+            }.bind(this)
+        ]);
+
+        aqueue.run(onLoadProgress, onLoadComplete);
+    };
+
+    return Loader;
+
+});
