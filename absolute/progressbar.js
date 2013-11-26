3c731f9a71da418b57ea8dbe9505f918bb8c15fc
Craig Robinson <craig@absoluteherogames.com>
2013-11-25 21:08:43 -0800

    added a waiting state to the progress bar

--- absolute/progressbar.js
+++ absolute/progressbar.js
@@ -5,7 +5,7 @@
  * Time: 6:26 PM
  * To change this template use File | Settings | File Templates.
  */
-define(['pixi'], function(PIXI) {
+define(['pixi', 'absolute/coords'], function(PIXI, Coords) {
 
     var ProgressBar = function(frameImage, fillImage, start, end) {
         this._initProgressBar(frameImage, fillImage, start, end);
@@ -53,6 +53,45 @@ define(['pixi'], function(PIXI) {
             frame.width = 1;
         }
         this.fillSprite.texture.setFrame(frame);
+        if (this.waitContainer) {
+            this.waitContainer.mask = null;
+            this.removeChild(this.waitContainer);
+            this.waitContainer = null;
+        }
+    };
+
+    ProgressBar.prototype.showWaiting = function (waitImage) {
+        this.waitImage = waitImage;
+        if (this.waitImage) {
+            this.setProgress(100);
+            this.waitContainer = new PIXI.DisplayObjectContainer();
+
+            this.waitSprites = [];
+            for (var i = 0; i < 11; i += 1) {
+                this.waitSprites.push(new PIXI.Sprite(this.waitImage));
+                this.waitSprites[i].position.x = i * this.waitSprites[i].width;
+                this.waitContainer.addChild(this.waitSprites[i]);
+            }
+            this.addChild(this.waitContainer);
+
+
+            var mask = new PIXI.Graphics();
+            mask.beginFill();
+            mask.drawRect(Coords.x(50), 0, this.width - Coords.x(100), this.height);
+            mask.endFill();
+            this.waitContainer.addChild(mask);
+            this.waitContainer.mask = mask;
+        }
+    };
+
+    ProgressBar.prototype.updateWaiting = function () {
+        for (var i = 0; i < 11; i += 1) {
+            this.waitSprites[i].position.x += Coords.x(4);
+        }
+        if (this.waitSprites[10].position.x > this.width) {
+            this.waitSprites[10].position.x = this.waitSprites[0].position.x - this.waitSprites[10].width;
+            this.waitSprites.unshift(this.waitSprites.pop());
+        }
     };
 
     return ProgressBar;
