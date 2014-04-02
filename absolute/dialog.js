52c37d471b6de48f8c60a8f7de5f1a14474eb2ed
jarbot <jared.gholston@absoluteherogames.com>
2014-04-02 09:45:14 -0700

    dialog using nineslice class

--- absolute/dialog.js
+++ absolute/dialog.js
@@ -1,6 +1,6 @@
-define(['pixi', 'absolute/screen', 'absolute/debug', 'lodash', 'absolute/button', 'absolute/audiomanager'],
+define(['pixi', 'absolute/screen', 'absolute/debug', 'lodash', 'absolute/button', 'absolute/audiomanager', 'absolute/nineslice'],
 
-    function(PIXI, Screen, Debug, _, Button, AudioManager) {
+    function(PIXI, Screen, Debug, _, Button, AudioManager, NineSlice) {
 
         var Dialog = function(ui, options) {
 
@@ -161,79 +161,9 @@ define(['pixi', 'absolute/screen', 'absolute/debug', 'lodash', 'absolute/button'
 
         Dialog.prototype._createBackground = function() {
 
-            var topLeft = PIXI.Sprite.fromFrame(this.options.images.topLeft);
-            topLeft.position.x = 0;
-            topLeft.position.y = 0;
+            var background = new NineSlice(this.options);
 
-            var topRight = PIXI.Sprite.fromFrame(this.options.images.topRight);
-            topRight.position.x = this.width - topRight.width;
-            topRight.position.y = 0;
-
-            var bottomLeft = PIXI.Sprite.fromFrame(this.options.images.bottomLeft);
-            bottomLeft.position.x = 0;
-            bottomLeft.position.y = this.height - bottomLeft.height;
-
-            var bottomRight = PIXI.Sprite.fromFrame(this.options.images.bottomRight);
-            bottomRight.position.x = this.width - bottomLeft.width;
-            bottomRight.position.y = this.height - bottomLeft.height;
-
-            // Tile middle left
-            var middleLeftSprite = new PIXI.Sprite.fromFrame(this.options.images.middleLeft),
-                middleLeftTile = new PIXI.TilingSprite(this.getTextureFromSpriteSheet(middleLeftSprite),
-                    middleLeftSprite.width,
-                    this.height - topLeft.height - bottomLeft.height
-                );
-            middleLeftTile.position.x = 0;
-            middleLeftTile.position.y = topLeft.height;
-
-            // Tile middle right
-            var middleRightSprite = new PIXI.Sprite.fromFrame(this.options.images.middleRight),
-                middleRightTile = new PIXI.TilingSprite(this.getTextureFromSpriteSheet(middleRightSprite),
-                    middleRightSprite.width,
-                    this.height - topLeft.height - bottomLeft.height
-                );
-            middleRightTile.position.x = this.width - topRight.width;
-            middleRightTile.position.y = topRight.height;
-
-            // Tile top center
-            var topCenterSprite = new PIXI.Sprite.fromFrame(this.options.images.topCenter),
-                topCenterTile = new PIXI.TilingSprite(this.getTextureFromSpriteSheet(topCenterSprite),
-                    this.width - topLeft.width - topRight.width,
-                    topCenterSprite.height
-                );
-            topCenterTile.position.x = topLeft.width;
-            topCenterTile.position.y = 0;
-
-            // Tile bottom center
-            var bottomCenterSprite = new PIXI.Sprite.fromFrame(this.options.images.bottomCenter),
-                bottomCenterTile = new PIXI.TilingSprite(this.getTextureFromSpriteSheet(bottomCenterSprite),
-                    this.width - bottomLeft.width - bottomRight.width,
-                    bottomCenterSprite.height
-                );
-            bottomCenterTile.position.x = bottomLeft.width;
-            bottomCenterTile.position.y = this.height - bottomLeft.height;
-
-
-            // Tile middle center
-            var middleCenterSprite = new PIXI.Sprite.fromFrame(this.options.images.middleCenter),
-                middleCenterTile = new PIXI.TilingSprite(this.getTextureFromSpriteSheet(middleCenterSprite),
-                this.width - topLeft.width - topRight.width,
-                Math.ceil(this.height - bottomRight.height - topRight.height)
-            );
-            middleCenterTile.position.x = topLeft.width;
-            middleCenterTile.position.y = topLeft.height;
-
-            this.container.addChild(topLeft);
-            this.container.addChild(topCenterTile);
-            this.container.addChild(topRight);
-
-            this.container.addChild(middleLeftTile);
-            this.container.addChild(middleCenterTile);
-            this.container.addChild(middleRightTile);
-
-            this.container.addChild(bottomLeft);
-            this.container.addChild(bottomCenterTile);
-            this.container.addChild(bottomRight);
+            this.container.addChild(background);
 
         };
 
