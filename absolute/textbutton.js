6607f8736d922b5009ff75b789880bfc8833d6ca
Lisa Wick <lisa@absoluteherogames.com>
2014-04-24 14:50:59 -0700

    merge

--- absolute/textbutton.js
+++ absolute/textbutton.js
@@ -1,50 +1,56 @@
 /**
  * Created by lisawick on 4/9/14.
  */
-define(['pixi','absolute/button', 'lodash', 'absolute/screenmetrics', 'absolute/platform'],
-    function (PIXI, Button, _, ScreenMetrics, Platform) {
+define(['pixi','absolute/button', 'absolute/threeslice', 'lodash', 'absolute/screenmetrics'],
+    function (PIXI, Button, ThreeSlice, _, ScreenMetrics) {
 
 
-    var TextButton = function(defaultImage, hoverImage, action, replaceOnHover, useTap, options) {
+    var TextButton = function(defaultImage, hoverImage, action, replaceOnHover, useTap, textStyleOptions, threeSliceOptions) {
 
-        var defaultOptions = {
+        var defaultTextStyleOptions = {
             text : "",
             textStyle : { font: Math.floor(90 * ScreenMetrics.getResScale()) + "px Ganache", align: "center" }
         };
 
-        this._initTextButton(defaultImage, hoverImage, action, replaceOnHover, useTap, _.extend(defaultOptions, options));
+        textStyleOptions = textStyleOptions || defaultTextStyleOptions;
+
+        if (typeof threeSliceOptions === 'object' ) {
+            this.container = new PIXI.DisplayObjectContainer();
+            defaultImage = this._createThreeSliceImage(threeSliceOptions);
+            hoverImage = this._createThreeSliceImage(threeSliceOptions);
+        }
+
+        this._initTextButton(defaultImage, hoverImage, action, replaceOnHover, useTap, _.extend(defaultTextStyleOptions, textStyleOptions));
     };
 
     TextButton.constructor = TextButton;
     TextButton.prototype = Object.create(Button.prototype);
 
-    TextButton.prototype._initTextButton = function(defaultImage, hoverImage, action, replaceOnHover, useTap, options) {
+    TextButton.prototype._initTextButton = function(defaultImage, hoverImage, action, replaceOnHover, useTap, textStyleOptions) {
 
         Button.call(this, defaultImage, hoverImage, action, replaceOnHover, useTap);
 
-        this.text = options.text;
-        this.textStyle = options.textStyle;
+        this.text = textStyleOptions.text;
+        this.textStyle = textStyleOptions.textStyle;
 
         this.label = new PIXI.BitmapText(this.text, this.textStyle);
         this.label.position.x = (this.width - this.label.textWidth) / 2;
         this.label.position.y = (this.height / 2) - this.label.textHeight;
         this.addChild(this.label);
+    };
 
-        if (!Platform.supportsTouch()) {
-            this.mouseover = function(evt) {
-                this.setTexture(this.hoverImage);
-                this.defaultY = this.position.y;
-                this.position.y = this.defaultY + 2;
-            };
-        }
+    TextButton.prototype._createThreeSliceImage = function(options) {
 
-        this.mouseout = function(evt) {
-            this.setTexture(this.defaultImage);
-            this.position.y = this.defaultY;
-        };
-     };
+        var threeSlice = new ThreeSlice(options);
 
+        var canvasRenderer = new PIXI.CanvasRenderer(threeSlice.width, threeSlice.height, null, true);
 
+        this.container.addChild(threeSlice);
+        canvasRenderer.render(threeSlice);
+        this.container.removeChild(threeSlice);
+
+        return PIXI.Texture.fromCanvas(canvasRenderer.view);
+    };
 
     return TextButton;
 
